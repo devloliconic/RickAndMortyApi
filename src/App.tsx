982b2945646b9f5001fb503c3db6d9504cf7d586
devloliconic <kirillevsmirnov@gmail.com>
2022-08-14 15:19:04 +0700

    feat: add first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import Character from "components/Character";
+import React, { useEffect, useState } from "react";
+
+import vk from "static/vk.png";
+
+import classes from "./App.module.css";
 
 function App() {
+  const [state, setState] = useState([]);
+  const fetchCharacter = async () => {
+    const response = await axios.get(
+      "https://rickandmortyapi.com/api/character"
+    );
+    setState(response.data.results);
+  };
+  useEffect(() => {
+    fetchCharacter();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={classes.page}>
+      <header className={classes.header}>
+        <div className={classes.container}>
+          <h1 className={classes.logo}>Rick And Morty Api</h1>
+        </div>
       </header>
+      <main className={classes.main}>
+        <div className={classes.container}>
+          <div className={classes.grid}>
+            {state.map((char) => (
+              <Character character={char} />
+            ))}
+          </div>
+        </div>
+      </main>
+      <footer className={classes.footer}>
+        <div className={classes.container}>
+          <div className={classes.footer_flex}>
+            <div className={classes.authorFlex}>
+              <img className={classes.icon} src={vk} alt="vkIcon" />
+
+              <a
+                href={"https://vk.com/salutkishukekaterine"}
+                className={classes.authorText}
+              >
+                Kirill smirnov
+              </a>
+            </div>
+            <a
+              className={classes.rickAndMoryLink}
+              href="https://rickandmortyapi.com/"
+            >
+              RickAndMortyApi
+            </a>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
---
+++ src/components/Character.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import classes from "./Character.module.css";
+interface CharacterProps {
+  character: {
+    name: string;
+    status: string;
+    species: string;
+    type: string;
+    gender: string;
+    image: string;
+  };
+}
+
+const Character = ({ character }: CharacterProps) => {
+  return (
+    <div className={classes.characterCard}>
+      <div className={classes.imgContainer}>
+        <img className={classes.img} src={character.image} alt="" />
+      </div>
+      <div className={classes.characterInfo}>
+        <p className={classes.text}>Name: {character.name}</p>
+        <p className={classes.text}>Status: {character.status}</p>
+        <p className={classes.text}>Species:{character.species}</p>
+        <p className={classes.text}>Gender: {character.gender}</p>
+        <p className={classes.text}>Type: {character.type}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Character;
